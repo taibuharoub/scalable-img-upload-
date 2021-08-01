af9dd1d52db81192c935c0135d849f8930c92934
Taibu Haroub <haroubtaibu@gmail.com>
2021-08-01 14:44:53 +0300

    Configure server

---
+++ cloudinar/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./utils/db");
+connectDB();
+const { upload } = require("./cloud");
+const app = express();
+
+const Image = require("./models/Image");
+
+app.post("/upload", upload, async (req, res, next) => {
+  try {
+    if (!req.file) {
+      const error = new Error("No image provided.");
+      error.statusCode = 422;
+      throw error;
+    }
+    const imageUrl = req.file.path;
+    console.log(imageUrl);
+    const image = new Image({
+      imageUrl,
+    });
+
+    await image.save();
+    res.status(200).json({
+      success: true,
+      imageUrl,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+app.listen(3000, () => {
+  console.log(`Server at http://localhost:3000`);
+});
