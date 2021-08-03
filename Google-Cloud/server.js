5bd953222acefdbab04e4680154959235d188fd2
Taibu Haroub <haroubtaibu@gmail.com>
2021-08-03 19:31:21 +0300

    Setup the http server

---
+++ Google-Cloud/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.post("/upload", (req, res, next) => {
+    //
+})
+
+app.listen(3000, () => {
+  console.log(`Server at http://localhost:3000`);
+});
